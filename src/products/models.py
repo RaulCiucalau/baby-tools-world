b9676fe34b558da38c577a8aa065e107f97e1cff
Raul Ciucalau <ciucalauraul@gmail.com>
2026-04-01 13:23:48 +0200

    Add Tag model and integrate product tagging functionality
    - Created Tag model with name and timestamps.
    - Updated Product model to include ManyToMany relationship with Tag.
    - Enhanced product detail and listing templates to display associated tags.
    - Added migrations for Tag model and updated Product model.

--- src/products/models.py
+++ src/products/models.py
@@ -22,6 +22,16 @@ class Category(models.Model):
         verbose_name_plural = "Categories"
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30, unique=True, null=False, blank=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    id = models.AutoField(primary_key=True)
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
 
     category = models.ForeignKey(Category, null=True, on_delete=models.DO_NOTHING)
@@ -29,6 +39,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to="imgs/products/", null=True, blank=True)
     name = models.CharField(max_length=80, blank=False, null=False)
     price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal("0.00"))])
+    tags = models.ManyToManyField(Tag, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
